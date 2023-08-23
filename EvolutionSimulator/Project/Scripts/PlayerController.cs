31c3431509caa94d9ad098edf26effb584c36319
AtleEng <atle.engelbrektsson@gmail.com>
2023-08-23 23:45:57 +0200

    f

--- EvolutionSimulator/Project/Scripts/PlayerController.cs
+++ EvolutionSimulator/Project/Scripts/PlayerController.cs
@@ -7,6 +7,21 @@ public class PlayerController : Component
     readonly float moveSpeed = 3;
     public override void Update()
     {
-        
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+        {
+            parentEntity.position.Y -= moveSpeed * Time.deltaTime;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+        {
+            parentEntity.position.Y += moveSpeed * Time.deltaTime;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            parentEntity.position.X += moveSpeed * Time.deltaTime;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+        {
+            parentEntity.position.X -= moveSpeed * Time.deltaTime;
+        }
     }
 }
